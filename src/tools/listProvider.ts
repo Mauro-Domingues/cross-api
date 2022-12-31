1ff8f80068fffd8688dd700b5d1c1e54bdcf24ea
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-12-31 17:00:41 -0300

    list-providers

--- src/tools/listProvider.ts
+++ src/tools/listProvider.ts
@@ -1,10 +1,56 @@
 export default function listProvider(): void {
   const providers = [
-    { Name: 'cache', Provider: 'CacheProvider' },
-    { Name: 'storage', Provider: 'StorageProvider' },
+    { Name: 'cache       ', Provider: 'CacheProvider       ' },
+    { Name: 'storage     ', Provider: 'StorageProvider     ' },
     { Name: 'mailTemplate', Provider: 'MailTemplateProvider' },
-    { Name: 'mail', Provider: 'MailProvider' },
+    { Name: 'mail        ', Provider: 'MailProvider        ' },
+    { Name: 'notification', Provider: 'notificationProvider' },
   ];
 
-  console.table(providers);
+  const trace = '\\';
+  console.log('');
+  console.log(
+    '\x1b[1m',
+    '\x1b[38;2;0;155;255m',
+    ` /==========================================================${trace}`,
+    '\x1b[0m',
+  );
+  console.log(
+    '\x1b[1m',
+    '\x1b[38;2;0;155;255m',
+    '|                                                            |',
+    '\x1b[0m',
+  );
+  providers.forEach(provider => {
+    console.log(
+      '\x1b[1m',
+      '\x1b[38;2;0;155;255m',
+      '|',
+      '\x1b[38;2;255;255;0m',
+      `   ➤  ${provider.Name}     `,
+      '\x1b[38;2;0;155;255m',
+      '|',
+      '\x1b[0m',
+      `${provider.Provider}`,
+      '\x1b[1m',
+      '\x1b[38;2;0;155;255m',
+      '       |',
+      '\x1b[0m',
+    );
+    console.log(
+      '\x1b[1m',
+      '\x1b[38;2;0;155;255m',
+      '|                                                            |',
+      '\x1b[0m',
+    );
+  });
+  console.log(
+    '\x1b[1m',
+    '\x1b[38;2;0;155;255m',
+    ` ${trace}==========================================================/`,
+    '\x1b[0m',
+  );
+  console.log('');
 }
+
+listProvider();
