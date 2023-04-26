4e6235a5e59dcbc456416434b62021bab2677ccb
Mauro-Domingues <maurinho.villa@hotmail.com>
2023-04-25 23:01:48 -0300

    fixes

--- src/templates/root/gitIgnore.ts
+++ src/templates/root/gitIgnore.ts
@@ -2,6 +2,7 @@ export class CreateGitIgnore {
   public execute(): string {
     return `# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
 
+/src/assets/errors.log
 /node_modules
 /coverage
 /.env
