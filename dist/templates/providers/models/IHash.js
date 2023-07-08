743a07f501738305440b6668540d4d6ffd9587c1
Mauro-Domingues <maurinho.villa@hotmail.com>
2023-07-08 12:45:36 -0300

    test

--- dist/templates/providers/models/IHash.js
+++
@@ -1,9 +0,0 @@
-export class CreateIHash {
-    execute() {
-        return `export interface IHashProviderDTO {
-  generateHash(payload: string): Promise<string>;
-  compareHash(payload: string, hashed: string): Promise<boolean>;
-}
-`;
-    }
-}
