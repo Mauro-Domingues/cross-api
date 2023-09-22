32cecc9495a540f6da790b7a9bd2a43c5d2c4398
Mauro-Domingues <maurinho.villa@hotmail.com>
2023-09-21 21:03:50 -0300

    release

--- src/templates/api/fakeDataSource.ts
+++ src/templates/api/fakeDataSource.ts
@@ -7,12 +7,13 @@ import 'reflect-metadata';
 export const FakeMysqlDataSource = {
   isInitialized: true,
   createQueryRunner: () => ({
-    isTransactionActive: true,
-    connect: async () => Promise.resolve(),
-    startTransaction: async () => Promise.resolve(),
-    commitTransaction: async () => Promise.resolve(),
     rollbackTransaction: async () => Promise.resolve(),
+    commitTransaction: async () => Promise.resolve(),
+    startTransaction: async () => Promise.resolve(),
+    connect: async () => Promise.resolve(),
     release: async () => Promise.resolve(),
+    isTransactionActive: true,
+    isReleased: false,
   }),
 } as DataSource;
 `;
