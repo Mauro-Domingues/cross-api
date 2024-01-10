1a90a99a655585e1727eb1f20bef73d8e8053256
Mauro-Domingues <maurinho.villa@hotmail.com>
2024-01-10 10:35:01 -0300

    fix

--- src/templates/assets/defaultLanguage.ts
+++ src/templates/assets/defaultLanguage.ts
@@ -2,7 +2,7 @@ export class CreateDefaultLanguage {
   public execute(languageData: string): string {
     return `export class Messages {
   messages;
-  public constructor() {
+  constructor() {
     this.messages = ${languageData}
   }
   execute() {
