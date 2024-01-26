07e6675f91da5b31edb8b2e004612795dccbb39d
Mauro-Domingues <maurinho.villa@hotmail.com>
2024-01-26 11:52:55 -0300

    tsconfig-fix

--- src/templates/root/tsConfig.ts
+++ src/templates/root/tsConfig.ts
@@ -2,11 +2,12 @@ export class CreateTsConfig {
   public execute(): string {
     return `{
   "compilerOptions": {
-    "target": "es6",
+    "target": "ES2022",
+    "module": "CommonJS",
+    "moduleResolution": "node",
     "lib": ["ES2022"],
-    "module": "commonjs",
     "outDir": "./dist",
-    "rootDir": ".",
+    "rootDir": "./src",
     "strict": true,
     "strictPropertyInitialization": false,
     "baseUrl": "./src",
@@ -17,19 +18,22 @@ export class CreateTsConfig {
       "@jobs/*": ["jobs/*"],
       "@middlewares/*": ["middlewares/*"],
       "@modules/*": ["modules/*"],
+      "@routes/*": ["routes/*"],
       "@shared/*": ["shared/*"],
       "@utils/*": ["utils/*"]
     },
     "esModuleInterop": true,
     "experimentalDecorators": true,
     "emitDecoratorMetadata": true,
-    "skipLibCheck": true,   
+    "skipLibCheck": true,
+    "noImplicitAny": true,
     "forceConsistentCasingInFileNames": true,
     "resolveJsonModule": true
   },
   "include": ["src"],
-  "exclude": ["node_modules", "dist"],
-  "ts-node": {"files": true}
-}`;
+  "exclude": ["node_modules", "dist", "**/*.spec.ts"],
+  "ts-node": { "files": true }
+}
+`;
   }
 }
