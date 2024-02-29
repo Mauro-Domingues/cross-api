eb4bc87f7f762f26bada72e4bffde957c577a12b
Mauro-Domingues <maurinho.villa@hotmail.com>
2024-02-29 18:43:40 -0300

    ready

--- src/templates/middlewares/ensureAuthenticated.ts
+++ src/templates/middlewares/ensureAuthenticated.ts
@@ -1,16 +1,16 @@
 export class CreateEnsureAuthenticated {
   public execute(): string {
-    return `import { readFileSync } ${'from'} 'node:fs';
-import { resolve } ${'from'} 'node:path';
+    return `// import { readFileSync } ${'from'} 'node:fs';
+// import { resolve } ${'from'} 'node:path';
 import { expressjwt } ${'from'} 'express-jwt';
 import { expressJwtSecret, GetVerificationKey } ${'from'} 'jwks-rsa';
-import { cryptoConfig } ${'from'} '@config/crypto';
+// import { cryptoConfig } ${'from'} '@config/crypto';
 
 export const ensureAuthenticated: ReturnType<typeof expressjwt> = expressjwt({
   secret: ((): Buffer | GetVerificationKey => {
-    if (process.env.NODE_ENV === 'test') {
-      return readFileSync(resolve(cryptoConfig.config.keysPath, 'public.pem'));
-    }
+    // if (process.env.NODE_ENV === 'test') {
+    //   return readFileSync(resolve(cryptoConfig.config.keysPath, 'public.pem'));
+    // }
     return expressJwtSecret({
       jwksUri: \`\${process.env.API_URL}/jwks\`,
       cache: true,
