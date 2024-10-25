641640ffda08516feb4f0c58bcbe63531ee90d37
Mauro-Domingues <maurinho.villa@hotmail.com>
2024-10-25 14:07:33 -0300

    ready-to-release

--- src/templates/assets/defaultConfig.ts
+++ src/templates/assets/defaultConfig.ts
@@ -3,19 +3,19 @@ export class CreateDefaultConfig {
     return `import { Messages } ${'from'} './messages.js';
 import { Console } ${'from'} './console.js';
 export class ConfigJson {
-    constructor() {
-        this.messages = Messages.getInstance().execute();
-        this.console = Console.getInstance();
-    }
-    execute() {
-        this.console.execute({
-            message: this.messages.comands.description.configured,
-            color: 'green',
-            bold: true,
-            breakStart: true,
-            breakEnd: true,
-        });
-    }
+  constructor() {
+    this.messages = Messages.getInstance().execute();
+    this.console = Console.getInstance();
+  }
+  execute() {
+    this.console.execute({
+      message: this.messages.comands.description.configured,
+      color: 'green',
+      bold: true,
+      breakStart: true,
+      breakEnd: true,
+    });
+  }
 }
 `;
   }
