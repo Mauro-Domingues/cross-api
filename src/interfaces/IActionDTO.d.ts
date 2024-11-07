aa6c6a93cbe963e154a9eb5bed766165e0dbf475
Mauro-Domingues <maurinho.villa@hotmail.com>
2024-11-07 14:10:54 -0300

    fix-interface

--- src/interfaces/IActionDTO.d.ts
+++ src/interfaces/IActionDTO.d.ts
@@ -3,12 +3,11 @@ import { ConfigJson } from '@tools/config';
 import { ConfigLanguage } from '@tools/languageConfig';
 import { ListProvider } from '@tools/listProvider';
 import { CreateApi } from '@tools/makeApi/index';
-import { CreateDependentModule } from '@tools/makeModule/dependent/index';
-import { CreateIndependentModule } from '@tools/makeModule/independent/index';
+import { CreateModule } from '@tools/makeModule/index';
 import { CreateProvider } from '@tools/makeProvider/index';
 
 export interface IActionDTO {
-  readonly 'make:module': () => CreateIndependentModule | CreateDependentModule;
+  readonly 'make:module': () => CreateModule;
   readonly 'make:provider': () => CreateProvider;
   readonly 'list:provider': () => ListProvider;
   readonly language: () => ConfigLanguage;
