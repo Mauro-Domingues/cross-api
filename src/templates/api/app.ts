a2705df369ebe95301a9011cba0a301ebbd67316
Mauro-Domingues <maurinho.villa@hotmail.com>
2024-12-17 16:22:35 -0300

    fixes

--- src/templates/api/app.ts
+++ src/templates/api/app.ts
@@ -3,18 +3,23 @@ export class CreateApp {
     return `import 'express-async-errors';
 import cluster ${'from'} 'node:cluster';
 import { cpus } ${'from'} 'node:os';
-import { setConnection } ${'from'} '@middlewares/setConnection';
-// import { storageConfig } ${'from'} '@config/storage'; // storageProvider
-// import { cryptoConfig } ${'from'} '@config/crypto'; // cryptoProvider
-import cors ${'from'} 'cors';
-import express, { Express } ${'from'} 'express';
-import { corsConfig } ${'from'} '@config/cors';
-import { parseParam } ${'from'} '@middlewares/parseParam';
-import { errorHandler } ${'from'} '@middlewares/errorHandler';
-import { rateLimiter } ${'from'} '@middlewares/rateLimiter';
+import express, {
+  Express,
+  json,
+  urlencoded,
+  // static as staticPath,
+} ${'from'} 'express';
 import { serve, setup } ${'from'} 'swagger-ui-express';
-import swaggerDocs ${'from'} '../swagger.json';
+import { setConnection } ${'from'} '@middlewares/setConnection';
+import { errorHandler } ${'from'} '@middlewares/errorHandler';
+import { parseParam } ${'from'} '@middlewares/parseParam';
+import { rateLimiter } ${'from'} '@middlewares/rateLimiter';
+import cors ${'from'} 'cors';
+import { corsConfig } ${'from'} '@config/cors';
+// import { cryptoConfig } ${'from'} '@config/crypto'; // cryptoProvider
+// import { storageConfig } ${'from'} '@config/storage'; // storageProvider
 import { routes } ${'from'} '../routes';
+import swaggerDocs ${'from'} '../swagger.json';
 import '@shared/container';
 
 export const app = new (class App {
@@ -32,15 +37,15 @@ export const app = new (class App {
     this.server.use(setConnection);
     this.server.use(cors(corsConfig));
     this.server.use(rateLimiter);
-    this.server.use(express.json());
-    this.server.use(express.urlencoded({ extended: true }));
+    this.server.use(json());
+    this.server.use(urlencoded({ extended: true }));
     this.server.use(parseParam);
   }
 
   private staticRoutes(): void {
     this.server.use('/doc', serve, setup(swaggerDocs));
-    // this.server.use('/uploads', express.static(storageConfig.config.uploadsFolder)); // storageProvider
-    // this.server.use('/jwks', express.static(cryptoConfig.config.jwksPath)); // expose public key feature
+    // this.server.use('/uploads', staticPath(storageConfig.config.uploadsFolder)); // storageProvider
+    // this.server.use('/jwks', staticPath(cryptoConfig.config.jwksPath)); // expose public key feature
   }
 
   private errorHandlers(): void {
@@ -51,20 +56,22 @@ export const app = new (class App {
     this.server.use(routes);
   }
 
+  private setupWorkers(): void {
+    Array.from({ length: cpus().length }).forEach(() => {
+      cluster.fork();
+    });
+
+    cluster.on('exit', () => {
+      cluster.fork();
+    });
+  }
+
   public init(): void {
     if (process.env.NODE_ENV === 'production' && cluster.isPrimary) {
-      const numCPUs = cpus().length;
-
-      Array.from({ length: numCPUs }).forEach(() => {
-        cluster.fork();
-      });
-
-      cluster.on('exit', () => {
-        cluster.fork();
-      });
+      this.setupWorkers();
     } else {
       this.server.listen(process.env.PORT, () => {
-        console.log('🚀 Server started on port %s!' process.env.PORT);
+        console.log('🚀 Server started on port %s!', process.env.PORT);
       });
     }
   }
