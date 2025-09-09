b417772a264f75f96151425afeeed6b331e12781
Mauro-Domingues <maurinho.villa@hotmail.com>
2025-09-09 15:34:50 -0300

    docker-compose-fixes

--- src/templates/root/dockerCompose.ts
+++ src/templates/root/dockerCompose.ts
@@ -5,77 +5,91 @@ export class CreateDockerCompose {
     container_name: app
     build: .
     restart: unless-stopped
-    ports:
-      - 3333:\${API_PORT}
+    networks:
+      - cross-network
+    depends_on:
+      mysql:
+        condition: service_healthy
+      redis:
+        condition: service_healthy
     env_file:
       - .env
     environment:
       - NODE_ENV=production
       - MYSQL_HOST=mysql
       - REDIS_HOST=redis
-    depends_on:
-      mysql:
-        condition: service_healthy
-      redis:
-        condition: service_healthy
     volumes:
       - jwks:/app/dist/assets/.well-known
       - keys:/app/dist/keys
       - tmp:/app/tmp
-    networks:
-      - cross-network
-
+    ports:
+      - 3333:\${API_PORT}
+    
   mysql:
     container_name: mysql
     image: mysql:latest
     restart: unless-stopped
-    ports:
-      - 3306:\${MYSQL_PORT}
+    networks:
+      - cross-network
     env_file:
       - .env
     volumes:
       - mysql:/var/lib/mysql
-    networks:
-      - cross-network
+    ports:
+      - 3306:\${MYSQL_PORT}
     healthcheck:
       test:
         - CMD
         - mysqladmin
         - ping
-        - '-p\${MYSQL_ROOT_PASSWORD}'
+        - -p\${MYSQL_ROOT_PASSWORD}
       interval: 10s
       timeout: 5s
       retries: 3
+    logging:
+      driver: json-file
+      options:
+        max-size: 100m
+        max-file: 10
 
   redis:
     container_name: redis
     image: bitnami/redis
     restart: unless-stopped
-    ports:
-      - 6379:\${REDIS_PORT}
-    env_file:
-      - .env
     networks:
       - cross-network
+    env_file:
+      - .env
+    ports:
+      - 6379:\${REDIS_PORT}
     healthcheck:
       test:
         - CMD
         - redis-cli
+        - -a
+        - \${REDIS_PASSWORD}
         - ping
-        - '-p\${REDIS_PASSWORD}'
       interval: 10s
       timeout: 5s
       retries: 3
+    logging:
+      driver: json-file
+      options:
+        max-size: 100m
+        max-file: 10
+
+volumes:
+  mysql:
+    driver: local
+  keys:
+    driver: local
+  jwks:
+    driver: local
+  tmp:
+    driver: local
 
 networks:
   cross-network:
-    driver: bridge
-
-volumes:
-  mysql: {}
-  keys: {}
-  jwks: {}
-  tmp: {}
-`;
+    driver: bridge`;
   }
 }
