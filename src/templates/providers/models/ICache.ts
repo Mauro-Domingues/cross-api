a6127bd51385835a91c6d8122515046e5fa19eb9
Mauro-Domingues <maurinho.villa@hotmail.com>
2026-06-16 11:36:29 -0300

    model-fix

--- src/templates/providers/models/ICache.ts
+++ src/templates/providers/models/ICache.ts
@@ -1,12 +1,17 @@
 export class CreateICache {
   public execute(): string {
     return `import type { IIntervalDTO } fr\u006Fm '@dtos/IIntervalDTO';
+import type { ICacheKeyDTO } fr\u006Fm '../dtos/ICacheKeyDTO';
 
 export interface ICacheProvider {
-  save<T>(key: string, value: T): Promise<void>;
-  saveTemporary<T>(key: string, value: T, ttl: IIntervalDTO): Promise<void>;
-  recovery<T>(key: string): Promise<T | null>;
-  invalidate(key: string): Promise<void>;
+  save<T>(key: ICacheKeyDTO, value: T): Promise<void>;
+  saveTemporary<T>(
+    key: ICacheKeyDTO,
+    value: T,
+    ttl: IIntervalDTO,
+  ): Promise<void>;
+  recovery<T>(key: ICacheKeyDTO): Promise<T | null>;
+  invalidate(key: ICacheKeyDTO): Promise<void>;
   invalidatePrefix(prefix: string): Promise<void>;
   close(): void;
 }
